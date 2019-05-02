80e712a6d887b4051e87494218bfd197919c7caf
ame <chenjiajie_1996@163.com>
2019-05-02 17:58:30 +0200

    second version

--- src/main/groovy/gl51/project/store/MemoryProductStorage.groovy
+++ src/main/groovy/gl51/project/store/MemoryProductStorage.groovy
@@ -3,34 +3,48 @@ package gl51.project.store
 
 class MemoryProductStorage implements  ProductStorage {
 	private Map<String, Product> productmap = [:]
+	private int id_total = 1
     @Override
     void save(Product p) {
+    	p.id = id_total
 		productmap[p.name] = p
+		id_total += 1
     }
-
+	
     @Override
     void update(String id, Product p) {
-
+    	Product product = getByID(id)
+    	if(product != null){
+    		productmap[product.name] = p
+    	}
     }
 
     @Override
     Product getByID(String id) {
-        return null
+    	for (e in productmap){
+    		if (e.value.id == id){
+    			Product product = e.value
+    			return product 
+    		}
+    	}
     }
 
     @Override
     void delete(String id) {
-
+		Product product = getByID(id)
+		if (product != null){
+			productmap.remove(product)
+		}
     }
 
     @Override
     List<Product> all() {
 		List<Product> list_result = []
 		for (e in productmap) {
-			list_result.add(e.value)
-			
+			list_result.add(e.value)	
 		}
 		print(productmap.size())
 		return list_result
     }
+    
 }
--- src/test/groovy/gl51/project/store/MemoryProductStorageTest.groovy
+++ src/test/groovy/gl51/project/store/MemoryProductStorageTest.groovy
@@ -22,5 +22,63 @@ class MemoryProductStorageTest extends Specification {
         all.size() == 1
         all.first().name == 'myproduct'
     }
+    def "adding a product will generate a new id"() {
+        setup:
+        Product myProduct = new Product(name: "myProduct")
+        def uuid = store.save(myProduct)
+
+        expect:
+        myProduct.id != null
+        myProduct.id == uuid
+    }
+
+    def "deleting a product will remove it from the list"() {
+        setup:
+        Product myProduct = new Product(name: "myProduct")
+        store.save(myProduct)
+
+        when:
+        store.delete(myProduct.getID())
+
+        then:
+        !store.all().contains(myProduct)
+    }
+
+    def "modifying a product will change it in the list"() {
+        setup:
+        Product myProduct = new Product(name: "myProduct")
+        store.save(myProduct)
+
+        when:
+        Product myUpdatedProduct = new Product(name: "myUpdatedProduct")
+        store.update(myProduct.getID(), myUpdatedProduct)
+
+        then:
+        myProduct != myUpdatedProduct
+    }
+
+    def "getting a product by its id will throw a NotExistingProductException if it doesn't exist"() {
+        setup:
+        def uuid = -1
+
+        when:
+        store.getByID(uuid)
+        
+        then:
+        thrown NotExistingProductException
+
+    }
+
+    def "getting a product by its id will return it if it does exist"() {
+        setup:
+        Product myProduct = new Product(name: "myProduct")
+        store.save(myProduct)
+
+        when:
+        Product gettedProduct = store.getByID(myProduct.getID())
+
+        then:
+        myProduct == gettedProduct
+    }
 		
 }
